85c1b55b3005c58e5443d8eb02bf1281373a85d6
Santiroca <111546253+Santiroca@users.noreply.github.com>
2022-11-22 16:11:04 -0300

    favoritos

--- js/favoritos.js
+++ js/favoritos.js
@@ -16,130 +16,114 @@ formulario.addEventListener("submit", function(event){
 //
 
 
-
+let imgUrlBase = `https://image.tmdb.org/t/p/original`
 
 
 
 let recuperoStorageSeries = localStorage.getItem("seriesFavs");
 let favoritosSeries = JSON.parse(recuperoStorageSeries);
 console.log(favoritosSeries);
+let sectionSeries = document.getElementById("seleccionSeries");
+let seriesTitle = document.getElementById('series')
 
 let recuperoStoragePeliculas = localStorage.getItem("peliculasFavs");
 let favoritosPeliculas = JSON.parse(recuperoStoragePeliculas);
 console.log(favoritosPeliculas);
+let sectionPeliculas = document.getElementById("seleccionPeliculas")
+let peliculasTitle = document.getElementById('pelis')
 
-
-let seccion = document.querySelector(".seleccionPeliculas");
-let sectionSeries = document.querySelector(".seleccionSeries");
-
-let qsObject = new URLSearchParams(location.search)
-let movie_id = qsObject.get("id")
-console.log(movie_id)
-let urlPeliculas = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=b3c4e9f716ea1c455601574fe492773b&language=en-US`;
-
-if (favoritosPeliculas == null || favoritosPeliculas.length == null){
-  sectionPeliculas.innerHTML = "No agregaste peliculas a la seccion de favoritos"
+if (favoritosPeliculas == null || favoritosPeliculas.length == 0){
+  peliculasTitle.innerHTML = "No agregaste peliculas a la seccion de favoritos"
 } else {
-  for (let i=0; i < favoritosPeliculas.length; i = i+1){
+  let allPeliculas = "";
 
-  
+  for (let i=0; i < favoritosPeliculas.length; i = i+1){
+    let movie_id = favoritosPeliculas[i]
+    console.log(movie_id)
+    let urlPeliculas = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=b3c4e9f716ea1c455601574fe492773b&language=en-US`;
+
     //recuperar y mostrar en pantalla los datos de cada pelicula favorita
 
-      fetch(urlPeliculas)
-      .then(function(res){
-          return res.json()
+    fetch(urlPeliculas)
+    .then(function(res){
+      return res.json()
     })
-      .then(function(data){
-        //Acá ya tenemmos los datos finales y es donde debemos escribir nuestro código.
-        console.log(data);
-        let arrayDePeliculas = data.results;
+    .then(function(data){
+      //Acá ya tenemmos los datos finales y es donde debemos escribir nuestro código.
+      console.log(data);
 
-        //1 Donde: Capturo el elemento html en donde quiero hacer una modificación
-        let seccion = document.querySelector('.seleccionPeliculas');
-        let allPeliculas = "";
-
-        //2 Qué: recorro la información de la api y la organizo para mostarla en el html
-        for(let i=0; i<arrayDePeliculas.length; i++){
-            //Dentro del for voy acumulando en la variable una estructura html por cada personaje del array.
-            allPeliculas += `<article>
-                                <img src=${arrayDePeliculas[i].image} alt='${arrayDePeliculas[i].name}' />
-                                <p> <a href="detalle.html?id=${arrayDePeliculas[i].id}"> Name: ${arrayDePeliculas[i].name} </p>
-                                <p>Status: ${arrayDePeliculas[i].status} </p>
-                            </article>`
-        }
-        //Con toda la estructura html completa ahora la paso al DOM
-        seccion.innerHTML = allPeliculas;
-
+      //1 Donde: Capturo el elemento html en donde quiero hacer una modificación
+      allPeliculas = `<li class="peliculas">
+                          <a class="nombres" href="detallepeliculas.html?id=${data.id}" </a>
+                          <img src="${imgUrlBase + data.poster_path}" class="peliculas"/>
+                          <h4>${data.title}</h4>
+                          <h5>${data.release_date}</h5>    
+                        </li>`
+      sectionPeliculas.innerHTML += allPeliculas;
+      //Con toda la estructura html completa ahora la paso al DOM
     })
     .catch( function(e){
         console.log(e)
     })
-     }
+  }
+  
     
 }
 
-
-let qsSerie = new URLSearchParams(location.search)
-let serie_id = qsSerie.get("id")
-console.log(serie_id)
-let urlSeries = `https://api.themoviedb.org/3/tv/${serie_id}?api_key=b3c4e9f716ea1c455601574fe492773b&language=en-US`;
-
-if (favoritosSeries == null || favoritosSeries.length == null){
-  sectionSeries.innerHTML = "No agregaste series a la seccion de favoritos"
+if (favoritosSeries === null || favoritosSeries.length == 0){
+  seriesTitle.innerHTML = "No agregaste series a la seccion de favoritos"
 } else {
-  for (let i=0; i < favoritosSeries.length; i = i+1);
-   //recuperar y mostrar en pantalla los datos de cada serie favorita
-}
+  let allSeries = "";
 
+  for (let i=0; i < favoritosSeries.length; i = i+1){
+    let serie_id = favoritosSeries[i]
+    console.log(serie_id)
+    let urlSeries = `https://api.themoviedb.org/3/tv/${serie_id}?api_key=b3c4e9f716ea1c455601574fe492773b&language=en-US`;
 
-      fetch(urlSeries)
-      .then(function(res){
-          return res.json()
+    //recuperar y mostrar en pantalla los datos de cada pelicula favorita
+
+    fetch(urlSeries)
+    .then(function(res){
+      return res.json()
     })
-      .then(function(data){
-        //Acá ya tenemmos los datos finales y es donde debemos escribir nuestro código.
-        console.log(data);
-        let arrayDeSeries = data.results;
+    .then(function(data){
+      //Acá ya tenemmos los datos finales y es donde debemos escribir nuestro código.
+      console.log(data);
 
-        //1 Donde: Capturo el elemento html en donde quiero hacer una modificación
-        let seccionSeries = document.querySelector('.seleccionSeries');
-        let allSeries = "";
-
-        //2 Qué: recorro la información de la api y la organizo para mostarla en el html
-        for(let i=0; i<arrayDeSeries.length; i++){
-            //Dentro del for voy acumulando en la variable una estructura html por cada personaje del array.
-            allSeries += `<article>
-                                <img src=${arrayDeSeries[i].image} alt='${arrayDeSeries[i].name}' />
-                                <p> <a href="detalle.html?id=${arrayDeSeries[i].id}"> Name: ${arrayDeSeries[i].name} </p>
-                                <p>Status: ${arrayDeSeries[i].status} </p>
-                            </article>`
-        }
-        //Con toda la estructura html completa ahora la paso al DOM
-        seccion.innerHTML = allSeries;
-
+      //1 Donde: Capturo el elemento html en donde quiero hacer una modificación
+      allSeries = `<li class="peliculas">
+                      <a class="nombres" href="detalleseries.html?id=${data.id}" </a>
+                        <img src="${imgUrlBase + data.poster_path}" class="peliculas"/>
+                        <h4>${data.name}</h4>
+                        <h5>${data.first_air_date}</h5>    
+                    </li>`
+      sectionSeries.innerHTML += allSeries;
+      //Con toda la estructura html completa ahora la paso al DOM
     })
     .catch( function(e){
         console.log(e)
     })
+  }  
+}
      
 
-    // Dark mode
+// Dark mode
 
-    let botonDark = document.querySelector('.botonDark');
+let botonDark = document.querySelector('.botonDark');
 
-    let body = document.querySelector('body')
+let body = document.querySelector('body')
+
+let inicio = document.querySelector('h2')
+
+botonDark.addEventListener('click', function(){
+    if(botonDark.innerText == 'Aclarar fondo'){
+    botonDark.innerText = 'Oscurecer fondo'
+    body.style.background = '#666566';    
+    } 
     
-    let inicio = document.querySelector('h2')
-    
-    botonDark.addEventListener('click', function(){
-        if(botonDark.innerText == 'Aclarar fondo'){
-        botonDark.innerText = 'Oscurecer fondo'
-        body.style.background = '#666566';    
-        } 
-        
-        else{
-    
-        body.style.background = '#000';
-        this.innerText = 'Aclarar fondo'
-        }
-      })
+    else{
+
+    body.style.background = '#000';
+    this.innerText = 'Aclarar fondo'
+    }
+  })
